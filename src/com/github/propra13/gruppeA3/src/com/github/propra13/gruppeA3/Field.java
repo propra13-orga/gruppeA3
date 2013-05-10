6fcbaf8cd3943558dc5efa9c46fd83081f4d988a
Vanessa Buerger <vanessa.buerger@uni-duesseldorf.de>
2013-05-10 23:02:50 +0200

    Projekt dungeoncrawlerA3
    importierfähiges Projekt in Eclipse

---
+++ src/com/github/propra13/gruppeA3/src/com/github/propra13/gruppeA3/Field.java
@@ -0,0 +1,90 @@
+package com.github.propra13.gruppeA3;
+
+public class Field {
+	
+	public Position pos = new Position();
+	public boolean hasLink = false;
+	
+	/* TODO:
+	 * Links
+	 * begehbar (statisch gemappt)
+	 */
+	boolean walkable = false;
+	final static int[] walkables
+	= {1, 3, 4, 5}; //TODO: Wasserverhalten klären
+
+	public enum fieldTypes {UNSET, BODEN, WAND, WASSER, TUER, LINK}
+	int type;
+	int texture; //für Abwechslung in Feldtexturen
+	int attribute1;
+	int attribute2;
+	int entityType;
+	int entityAttr;
+	
+	public Link link = null;
+	
+	
+	public Field(
+			int type, int texture, int attr1, int attr2, int entityType, int entityAttr, Position pos) {
+		
+		this.type = type;
+		this.texture = texture;
+		this.attribute1 = attr1;
+		this.attribute2 = attr2;
+		this.entityType = entityType;
+		this.entityAttr = entityAttr;
+		this.pos = pos;
+		
+		//Walkable setzen
+		for (int i=0; i < walkables.length; i++) {
+			if (walkables[i] == type) {
+				walkable = true;
+				break;
+			}
+		}
+	}
+	
+	public String fieldType () {
+		switch (this.type) {
+		case 0:
+			return "unset";
+		case 1:
+			return "Boden";
+		case 2:
+			return "Wand";
+		case 3:
+			return "Wasser";
+		case 4:
+			return "Tuer";
+		default:
+			return "unknown";
+		}
+	}
+	
+	public char charMap () {
+		switch (this.fieldType()) {
+		
+		case "unset":
+			return 'X';
+			
+		case "Boden":
+			return 'O';
+			
+		case "Wand":
+			return '#';
+			
+		case "Wasser":
+			if (this.attribute1 == 1) return 'H';
+			else return '≈';
+			
+		case "Tuer":
+			return '+';
+			
+		case "Spawn":
+			return '%';
+			
+		default:
+			return 'X';
+		}
+	}
+}
---
+++ src/com/github/propra13/gruppeA3/src/com/github/propra13/gruppeA3/Player.java
@@ -0,0 +1,47 @@
+package com.github.propra13.gruppeA3;
+
+import com.github.propra13.gruppeA3.Moveable;
+
+/**
+ *  
+ * @author Majida Dere
+ * Diese Klasse definiert einen Spieler und seine Eigenschaften und Methoden.
+ * 
+ */
+
+public class Player extends Moveable {
+// Attribute
+	int life;//Lebensstärke?
+	int power;//Figurstärke
+	direction direct= direction.NONE; //Richtung
+	
+	// Methode zum Bewegen von Objekten
+		public void move(){
+			switch(direct){
+				case LEFT: if(leftFree(pos.x-1,pos.y)){
+					setPosition(pos.x-1,pos.y);
+					//feldNeuZeichnen();
+					//zusätzliche Monsteraktionen/Zufallserscheininungen von Gegenständen
+				}break;
+				case RIGHT: if(rightFree(pos.x+1,pos.y)){
+					setPosition(pos.x+1,pos.y);
+					//feldNeuZeichnen();
+					//zusätzliche Monsteraktionen/Zufallserscheininungen von Gegenständen
+				}break;
+				case UP: if (upFree(pos.x,pos.y+1)){
+					setPosition(pos.x,pos.y+1);
+					//feldNeuZeichnen();
+					//zusätzliche Monsteraktionen/Zufallserscheininungen von Gegenständen
+				}break;
+				case DOWN:if (downFree(pos.x,pos.y-1)){
+					setPosition(pos.x,pos.y+1);
+					//feldNeuZeichnen();
+					//zusätzliche Monsteraktionen/Zufallserscheininungen von Gegenständen
+				}break;
+				case NONE: //zusätzliche Monsteraktionen/Zufallserscheininungen von Gegenständen
+					
+			}
+		}	
+		
+		
+}
