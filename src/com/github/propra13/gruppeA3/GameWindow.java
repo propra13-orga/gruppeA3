031ef148ba2d940966a160c984d2b3eb8137ce0b
Jenny Lenz <Jenny.Lenz@uni-duesseldorf.de>
2013-05-20 01:16:43 +0200

    Erste Beschreibung des GameWindows

---
+++ src/com/github/propra13/gruppeA3/GameWindow.java
@@ -0,0 +1,34 @@
+﻿package com.github.propra13.gruppeA3;
+
+/**
+ * Klasse für GUI: Spielgrafik
+ * @autor Jenny Lenz
+ */
+
+import javax.swing.*;
+import java.awt.*;
+
+public class GameWindow extends JFrame{
+
+
+
+    public GameWindow() {
+
+    }
+
+    public void paint(Graphics g) {
+
+        // Allgemeine Karte zeichnen
+        // Player Zeichnen
+
+        while(true) {
+            // Bewegungen abfangen
+
+            // Auf Bewegungen reagieren
+            // ggf. Aktionenen ausführen (Bewegen)
+            // nach der Aktion das betroffene Objekt neu zeichnen
+        }
+
+    }
+
+}
