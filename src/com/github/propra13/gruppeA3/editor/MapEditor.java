ebb7cc4f8831a46606d6197250ff53aa2583ce81
Christian Krüger <christian.krueger@hhu.de>
2013-05-24 18:51:37 +0200

    Grundstein Karteneditor

---
+++ src/com/github/propra13/gruppeA3/editor/MapEditor.java
@@ -0,0 +1,15 @@
+package com.github.propra13.gruppeA3.editor;
+
+import javax.swing.JFrame;
+
+/* @author CK
+ * Hauptklasse des Map-Editors
+ */
+public class MapEditor extends JFrame {
+	
+	private static final long serialVersionUID = 1L;
+
+	public MapEditor() {
+		
+	}
+}
