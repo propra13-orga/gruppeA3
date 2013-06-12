a9b83ebc73fff1d90de88807728aeb2549b42aaf
Vanessa Bürger <vanessa.buerger@uni-duesseldorf.de>
2013-06-12 18:46:14 +0200

    unnötige Klassen gelöscht

--- src/com/github/propra13/gruppeA3/Entities/Player.java
+++ src/com/github/propra13/gruppeA3/Entities/Player.java
@@ -14,7 +14,6 @@ import com.github.propra13.gruppeA3.Map.Map;
 import com.github.propra13.gruppeA3.Map.Position;
 import com.github.propra13.gruppeA3.Map.Room;
 import com.github.propra13.gruppeA3.Map.Trigger;
-import com.github.propra13.gruppeA3.Menu.GameEndWindow;
 import com.github.propra13.gruppeA3.Menu.MenuStart;
 
 /**
