c71106ca23a8b0d1ded950ac6e587e93f53efa3f
Christian Krüger <christian.krueger@hhu.de>
2013-06-13 14:16:01 +0200

    git add . vergessen

---
+++ src/com/github/propra13/gruppeA3/Entities/Buff.java
@@ -0,0 +1,10 @@
+package com.github.propra13.gruppeA3.Entities;
+
+public abstract class Buff {
+
+	/** Wird jeden Gametick aufgerufen
+	 * Nutzung: Zeit herunterzählen oä
+	 * @param time Geschwindigkeit des Timers an
+	 */
+	public abstract void tick();
+}
---
+++ src/com/github/propra13/gruppeA3/Entities/SpeedBuff.java
@@ -0,0 +1,30 @@
+package com.github.propra13.gruppeA3.Entities;
+
+public class SpeedBuff extends Buff{
+	
+	private int tickCounter;
+	final public static int manaCost = 15;
+	Player player;
+	
+	/**
+	 * Setzt einen Speedbuff für den Player.
+	 * @param player Spieler, der den Buff bekommt
+	 * @param factor Speed-Faktor, der gesetzt werden soll
+	 * @param time Dauer, die der Speed-Buff halten soll
+	 */
+	public SpeedBuff(Player player, int factor, int time) {
+		this.player = player;
+		player.setSpeed(factor);
+		tickCounter = (int)((double)(1000.0 / 17/*MenuStart.delay*/)) * time;
+		player.setMana(player.getMana() - manaCost);
+	}
+	
+	@Override
+	public void tick() {
+		tickCounter--;
+		if(tickCounter == 0) {
+			player.buff = null;
+			player.setSpeed(1);
+		}
+	}
+}
