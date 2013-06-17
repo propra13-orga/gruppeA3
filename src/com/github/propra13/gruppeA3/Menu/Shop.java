1445fa8d957f8e2fb12387fa274a1e65f4d96101
majidadere <majida.dere@uni-duesseldorf.de>
2013-06-18 01:16:40 +0200

    Shop lässt sich nun aufrufen.

--- src/com/github/propra13/gruppeA3/Menu/Shop.java
+++ src/com/github/propra13/gruppeA3/Menu/Shop.java
@@ -1,12 +1,15 @@
 package com.github.propra13.gruppeA3.Menu;
 
+import javax.swing.BorderFactory;
+import javax.swing.BoxLayout;
 import javax.swing.DefaultListModel;
+import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JButton;
+import javax.swing.JTextField;
 import javax.swing.ListSelectionModel;
 
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
+import java.awt.BorderLayout;
 import java.util.Iterator;
 import java.util.LinkedList;
 
@@ -18,8 +21,12 @@ import javax.swing.event.ListSelectionListener;
 import com.github.propra13.gruppeA3.Entities.Item;
 import com.github.propra13.gruppeA3.Entities.NPC;
 import com.github.propra13.gruppeA3.Entities.Player;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.Window.Type;
+import java.awt.Dialog.ModalExclusionType;
 
-public class Shop extends JPanel 
+public class Shop extends JFrame 
 				  implements ListSelectionListener{
 
 	/**
@@ -29,13 +36,25 @@ public class Shop extends JPanel
 	private LinkedList<Item> items;
 	private JList list;
 	private DefaultListModel listModel;
+    private static final String buyString = "Kaufen";
+    private static final String sellString = "Verkaufen";
+    private JButton buyButton;
+	private JButton sellButton;
+    private JTextField text;
 	
-	private static final String buyString = "Kaufen";
 
 	/**
 	 * Create the panel.
 	 */
 	public Shop(Player player, NPC npc) {
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		setAlwaysOnTop(true);
+		setResizable(false);
+		setType(Type.UTILITY);
+		setTitle(npc.getName()+"s Shop");
+		setSize(450,300);
+		setLocationRelativeTo(null);
+		//setBounds(100, 100, 450, 300);
 		
 		items = npc.getItems();
 		Item item;
@@ -44,22 +63,48 @@ public class Shop extends JPanel
 		
 		while(iter.hasNext()){
 			item = (Item)iter.next();
-			listModel.addElement(item.getName()+" "+item.getDesc()+" - "+item.getValue()+" Coins");
+			listModel.addElement(item.getName()+"       "+item.getDesc()+"   -    "+item.getValue()+" Coins");
 		}
 		
         //Liste mit Scrolling.
         list = new JList(listModel);
-        list.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+        list.setSelectionMode(ListSelectionModel.MULTIPLE_INTERVAL_SELECTION);
         list.setSelectedIndex(0);
         list.addListSelectionListener(this);
-        list.setVisibleRowCount(10);
+        list.setVisibleRowCount(5);
         JScrollPane listScrollPane = new JScrollPane(list);
-		listScrollPane.setViewportView(list);
-        
-		JButton btnNewButton = new JButton(buyString);
-		add(listScrollPane);
-		add(btnNewButton);
-		this.setVisible(true);
+ 
+        sellButton = new JButton("Verkaufen");
+        sellButton.addActionListener(new ActionListener() {
+        	public void actionPerformed(ActionEvent e) {
+        	}
+        });
+
+        sellButton.setActionCommand(sellString);
+        sellButton.setEnabled(false);
+ 
+        buyButton = new JButton("Kaufen");
+        buyButton.addActionListener(new ActionListener() {
+        	public void actionPerformed(ActionEvent e) {
+        	}
+        });
+ 
+        buyButton.setActionCommand(buyString);
+ 
+        text = new JTextField(10);
+
+ 
+        JPanel buttonPane = new JPanel();
+        buttonPane.setLayout(new BoxLayout(buttonPane,
+                                           BoxLayout.LINE_AXIS));
+        buttonPane.add(buyButton);
+        buttonPane.add(text);
+        buttonPane.add(sellButton);
+        buttonPane.setBorder(BorderFactory.createEmptyBorder(5,5,5,5));
+ 
+        getContentPane().add(listScrollPane, BorderLayout.CENTER);
+        getContentPane().add(buttonPane, BorderLayout.PAGE_END);
+        setVisible(true);
 	}
 
 	@Override
